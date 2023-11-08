77dc7862effd812ccbc61550607a1bc828f8bb63
Cristiane Martins <crisplus@gmail.com>
2023-11-08 14:33:20 -0300

    Add logo, logo styles, body styles, etc. Lots off changes were made but not commited

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import styles from './Header.module.css'
-
+import { Logo } from '../assets/logo';
 
 export function Header() {
     return (
+        
         <header className={styles.header}>
-        <strong>todo</strong>
+            
+            <Logo />
+        
         </header>
     );  
 }
